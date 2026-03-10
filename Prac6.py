a2c065b91be17a248b87405f3de90f6eb11e864d
Harshwardhan Chauhan <harsh15022003@gmail.com>
2026-03-10 07:51:56 +0530

    Update Prac6.py

--- Prac6.py
+++ Prac6.py
@@ -7,3 +7,27 @@ while True:
  t=time.localtime()
  display.ShowClock(t.tm_hour,t.tm_min)
  time.sleep(1)
+
+//2
+import tm1637
+import time
+
+display = tm1637.TM1637(clk=21, dio=20)
+
+while True:
+    t = time.localtime()
+    hour = t.tm_hour
+    minute = t.tm_min
+
+
+
+    display.numbers(hour, minute)
+    time.sleep(1)
+
+
+//3
+import tm1637
+
+display = tm1637.TM1637(clk=21, dio=20)
+
+display.show('8888') 
